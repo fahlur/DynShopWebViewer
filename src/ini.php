3f053eb939e648985bdeb741b3f95b7968ba98fc
Fahlur <ti07shadow@gmail.com>
2015-09-24 08:09:12 -0500

    Create ini.php

---
+++ src/ini.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+###############################################################
+########## UPDATE DATABASE CONNECTION VARIABLES HERE ##########           
+###############################################################
+
+# Set your Server ip here.
+define("HOSTADDR", "HOST_ADDRESS_HERE");
+
+# Set your Database name here.
+define("DATABASE", "DATABASE_HERE");
+
+# Set your Database username here.
+define("USERNAME", "USERNAME_HERE");
+
+# Set your Database password here.
+define("PASSWORD", "PASSWORD_HERE");
+
+
+###############################################################
+############ ADDITIONAL SETTINGS & VARIABLES HERE #############           
+###############################################################
+
+# This controls what kind of items you want listed
+# OP_ALL = Shows every item even if not in shop
+# ALL = List all Items Possible, In Shop
+# NONBUY = Dont list unbuyable items (Recommended)
+# NONSELL = Dont list unsellable items
+
+define("LIST_ITEMS", "NONBUY");
+
+
+?>
